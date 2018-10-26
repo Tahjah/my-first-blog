a6befb8e1d8c7e9401d8f7f0d45eefa5b0f9fa7d
Tahjah-Alize <tahjah@potatolondon.com>
2018-10-26 15:14:20 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,18 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+    from django.shortcuts import render, get_object_or_404
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+    from .forms import PostForm
+    def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+    from django.shortcuts import render, get_object_or_404
+    def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
